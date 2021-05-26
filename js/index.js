4720ddab26062db07a85daa44328f4b3a5442346
byron7cueva <byron7cueva@gmail.com>
2021-05-26 14:07:40 -0500

    Html layout of the Range component

--- js/index.js
+++ js/index.js
@@ -1,6 +1,27 @@
 'use strict'
 
+class Range extends React.Component {
+  render() {
+    return (
+      <div className="range">
+        <div className="range__value range__value-min">0$</div>
+        <div className="slider">
+          <div className="slider__bar">
+            <div className="range__area range__area-min">
+              <div className="bullet"></div>
+            </div>
+            <div className="range__area range__area-max">
+              <div className="bullet"></div>
+            </div>
+          </div>
+        </div>
+        <div className="range__value range__value-max">100$</div>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <h1>Hola mundo</h1>,
+  <Range />,
   document.getElementById('root')
 );
